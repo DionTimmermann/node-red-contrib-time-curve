e339df7b18618e7f65a8e9783708635a1ac9e8bc
DionTimmermann <mail@dion-timmermann.de>
2025-10-26 07:53:30 +0100

    Reworked most of the code. Now functional except when editing list directly.

--- js-spline-curve/index.js
+++
@@ -1,2 +0,0 @@
-exports.Curve = require("./src/Curve").Curve;
-
--- time-curve.js
+++ time-curve.js
@@ -1,10 +1,8 @@
-Curve = require('./Curve').Curve;
+const secondsPerDay = 24 * 60 * 60;
 
 module.exports = function(RED) {
-    function splineCurve(config) {
+    function timeCurve(config) {
         RED.nodes.createNode(this, config);
-
-        const curve = new Curve(config);
 
         const iterableAssignment = function(obj, keys, value){
             if(keys.length == 1){
@@ -19,6 +17,13 @@ module.exports = function(RED) {
             }
         }
 
+        // Clone and extend points cyclically
+        this.points = [
+            { x: config.points[config.points.length - 1].x - secondsPerDay, y: config.points[config.points.length - 1].y },
+            ...config.points,
+            { x: config.points[0].x + secondsPerDay, y: config.points[0].y }
+        ];
+
         var node = this;
         node.on('input', function(msg){
             const input_key  = (config.input_key)?config.input_key:"payload";
@@ -26,16 +31,43 @@ module.exports = function(RED) {
             const inputKeys  = input_key.split(".");
             const outputKeys = output_key.split(".");
 
-            var inputValue = msg;
+            let inputValue = msg;
             for( i=0; i < inputKeys.length; i++ ){
                 inputValue = inputValue[inputKeys[i]];
             }
 
-            const result = curve.getValueAt(inputValue);
+            const date = new Date(inputValue);
+            const seconds = (
+                date.getHours() * 3600 +
+                date.getMinutes() * 60 +
+                date.getSeconds() +
+                date.getMilliseconds() / 1000
+            );
+
+            // Find segment [p0, p1] such that p0.x <= seconds < p1.x
+            let p0, p1;
+            for (let i = 0; i < node.points.length - 1; i++) {
+                if (seconds >= node.points[i].x && seconds < node.points[i + 1].x) {
+                    p0 = node.points[i];
+                    p1 = node.points[i + 1];
+                    break;
+                }
+            }
+
+            // Handle edge case (seconds exactly at the end)
+            if (!p0) {
+                p0 = node.points[node.points.length - 2];
+                p1 = node.points[node.points.length - 1];
+            }
+
+            // Linear interpolation
+            const t = (seconds - p0.x) / (p1.x - p0.x);
+            const result = p0.y + t * (p1.y - p0.y);
+
             iterableAssignment(msg, outputKeys, result);
             node.send(msg);
         });
     }
-    RED.nodes.registerType("spline-curve", splineCurve);
+    RED.nodes.registerType("time-curve", timeCurve);
 }
 
